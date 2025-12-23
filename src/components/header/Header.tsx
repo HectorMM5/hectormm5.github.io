b1631ca38d2c98cfc818a65f7c0c79b781d16f60
HectorMM5 <hektor.mendana@gmail.com>
2025-12-23 13:59:28 +0100

    Simple header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import './Header.css'
+
+function header() {
+
+    return (
+        <div className="headerBlock">
+            <h1>Hector Mendana Morales</h1>
+            <h3>A presentation</h3>
+        </div>
+        
+    )
+
+}
+
+export default header;
