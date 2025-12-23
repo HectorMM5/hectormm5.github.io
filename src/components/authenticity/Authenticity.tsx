87e579f62e0fda7db9c3ad6a469033135faf3f48
HectorMM5 <hektor.mendana@gmail.com>
2025-12-23 14:17:43 +0100

    Basic auth section

---
+++ src/components/authenticity/Authenticity.tsx
@@ -0,0 +1,15 @@
+import './Authenticity.css'
+
+function authenticity() {
+
+    return (
+        <div className="authenticity">
+            <h2>About authenticity</h2>
+            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto cum fuga porro rerum, deserunt odit laboriosam necessitatibus laborum, alias natus quasi repellendus? Harum dolorem distinctio itaque ipsa.</h4>
+        </div>
+        
+    )
+
+}
+
+export default authenticity;
