5a52168bae291e9e5a59fc331838269773631576
Hector <hektor.mendana@gmail.com>
2026-01-07 21:08:21 +0100

    Modified look

--- src/components/TitleAndText/TitleAndText.tsx
+++ src/components/TitleAndText/TitleAndText.tsx
@@ -1,10 +1,10 @@
-import './Authenticity.css'
+import './TitleAndText.css'
 
 function authenticity() {
 
     return (
         <div className="authenticity">
-            <h2>About authenticity</h2>
+            <h2>Authenticity</h2>
             <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto cum fuga porro rerum, deserunt odit laboriosam necessitatibus laborum, alias natus quasi repellendus? Harum dolorem distinctio itaque ipsa.</h4>
         </div>
         
