1dcb6a3e041c5242ced38147d880271b1fc857b2
Hector <hektor.mendana@gmail.com>
2026-01-07 20:53:32 +0100

    added components

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import Header from './components/header/Header.tsx'
+import Topbar from './components/topbar/Topbar.tsx'
+import TitleAndText from './components/titleAndText/TitleAndText.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Topbar />
+
+    <TitleAndText />
     <Header />
   </StrictMode>,
 )
