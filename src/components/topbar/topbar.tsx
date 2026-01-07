b4a161108f66e0a291977d562f4ea0f3af9ae771
Hector <hektor.mendana@gmail.com>
2026-01-07 20:47:40 +0100

    topbar imported from previous project

---
+++ src/components/topbar/topbar.tsx
@@ -0,0 +1,24 @@
+
+
+
+import './topbar.css'
+
+function topbar() {
+
+  return (
+    <header className="topbar">
+
+      <p>Hector</p>
+
+      <div className="barButtons">
+        <a href="">GitHub</a>
+      </div>
+
+    </header>
+  )
+
+}
+
+export default topbar;
+
+
