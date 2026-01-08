3c6d4931a910d58d78d8085f3bc954380db07f24
HectorMM5 <hektor.mendana@gmail.com>
2026-01-08 14:27:30 +0100

    created button

---
+++ src/components/navButton/NavButton.tsx
@@ -0,0 +1,14 @@
+import './splitRow.css'
+
+function NavButton({ name }: { name: string }) {
+
+    return (
+        <div className="navButton">
+            {name}
+        </div>
+        
+    )
+
+}
+
+export default NavButton;
