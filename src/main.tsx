9cac9019ecaa649fd513b1ae2a455acf060071dc
HectorMM5 <hektor.mendana@gmail.com>
2026-01-08 14:27:46 +0100

    changed import

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './components/header/Header.tsx'
-import Topbar from './components/topbar/Topbar.tsx'
-import TitleAndText from './components/titleAndText/TitleAndText.tsx'
+import Topbar from './components/topbar/topbar.tsx'
+import TitleAndText from './components/TitleAndText/TitleAndText.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
