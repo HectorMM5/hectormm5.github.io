d136f014161b5238a6e0030bb36462cb5f52f61e
Hector <hektor.mendana@gmail.com>
2026-02-18 18:47:39 +0100

    Added experience component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Topbar from './components/Topbar.vue'
 import Hero from './components/Hero.vue'
+import Experience from './components/Experience.vue'
 </script>
 
 <template>
   <Topbar />
   <Hero />
+  <Experience />
 </template>
 
 <style scoped></style>
