66cbea9046eef4facc2841da295439559123797c
Hector <hektor.mendana@gmail.com>
2026-02-18 14:40:04 +0100

    Styling

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,13 +5,14 @@
             <h3>Computer Science @ NTNU</h3>
             <div class="separator"></div>
             <h3>Developer @ MAKE NTNU</h3>
-
+            <h4>Java | Python | TypeScript | C#</h4>
+            
         </div>
         <div class="right-side">
             <h3 class="contact">hmmoralesdev@gmail.com</h3>
-            <h4 class="GitHub">GitHub</h4>
+            <h3 class="GitHub">GitHub</h3>
         </div>
-    </div>
+    </div> 
 </template>
 
 <script>
@@ -22,7 +23,8 @@ export default {
 
 <style scoped>
 .hero {
-    margin: 10% 10%;
+    margin: 3% 3%;
+    padding: 1rem;
     display: flex;
     flex-direction: row;
     background-color: rgb(20, 20, 20);
@@ -38,17 +40,27 @@ export default {
     display: block;
     font-family: 'Courier New', Courier, monospace;
     font-size: 2rem;
+    color: white;
 }
 
-.left-side h2 {
+.left-side h3 {
     display: block;
     font-family: 'Courier New', Courier, monospace;
     font-size: 1rem;
+    color: white;
+}
+
+.left-side h4 {
+    display: block;
+    font-family: 'Courier New', Courier, monospace;
+    font-size: 0.7rem;
+    color: white;
 }
 
 .left-side .separator {
-    background-color: rgb(22, 22, 22);
+    border: solid rgb(255, 0, 0);
     border-radius: 5px;
+    width: 30%;
 }
 
 .right-side {
@@ -57,14 +69,14 @@ export default {
     height: 100%;
     width: 30%;
     align-items: baseline;
+    margin-top: auto;
 }
 
-.right-side h2 {
+.right-side h3 {
     display: block;
     font-family: 'Courier New', Courier, monospace;
     font-size: 0.7rem;
+    color: white;
 }
-
-
 
 </style>
