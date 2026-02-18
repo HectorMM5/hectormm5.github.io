00390baa8523eec60d2362f7508655da3af4c288
Hector <hektor.mendana@gmail.com>
2026-02-18 19:10:48 +0100

    Added WIP notif

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import Topbar from './components/Topbar.vue'
 import Hero from './components/Hero.vue'
 import Experience from './components/Experience.vue'
+import WIPNotif from './WIPNotif.vue'
 </script>
 
 <template>
   <Topbar />
   <Hero />
   <Experience />
+  <WIPNotif />
 </template>
 
 <style scoped></style>
