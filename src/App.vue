0441b29f71b103f2b1d7a1d98a26a9778f6bd5f1
Hector <hektor.mendana@gmail.com>
2026-02-18 15:12:23 +0100

    usage of components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
-import Topbar from "./components/Topbar.vue";
-import Hero from "./components/Hero.vue";
-
+import Topbar from './components/Topbar.vue'
+import Hero from './components/Hero.vue'
 </script>
 
 <template>
   <Topbar />
   <Hero />
-
 </template>
 
 <style scoped></style>
