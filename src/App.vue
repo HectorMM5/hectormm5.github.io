f865734582f8ba336073ec645e3d05f6636eab87
Hector <hektor.mendana@gmail.com>
2026-02-19 17:58:20 +0100

    Added Projects tab

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import Topbar from './components/Topbar.vue'
 import Hero from './components/Hero.vue'
 import Experience from './components/Experience.vue'
 import WIPNotif from './WIPNotif.vue'
+import Projects from './components/Projects.vue';
 </script>
 
 <template>
   <Topbar />
   <Hero />
   <Experience />
+  <Projects />
   <WIPNotif />
 </template>
 
