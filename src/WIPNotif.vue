a2e721596a1fc67004fcc6dbcf15ba5d69793d04
Hector <hektor.mendana@gmail.com>
2026-02-19 21:45:23 +0100

    Temporary size safeguard for mobile

--- src/WIPNotif.vue
+++ src/WIPNotif.vue
@@ -20,7 +20,9 @@ export default {
   border-radius: 8px 8px 0 0;
   box-shadow: 0 -2px 10px rgba(0, 0, 0, 0.3);
   font-weight: 500;
-  font-size: 2rem;
+  font-size: 1.5rem;
   z-index: 1000;
+
+  max-height: 10dvh;
 }
 </style>
