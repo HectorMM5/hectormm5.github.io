588bf4efa621d8fc40e90bbe0d6c14637c78abed
Hector <hektor.mendana@gmail.com>
2026-03-10 19:37:22 +0100

    added wrapper

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="hero">
+
     <Peaks />
 
-    <div class="left-side"> 
-      <h1 class="name">Hector Mendana Morales</h1>
-      <h3>Computer Science @ NTNU</h3>
-      <div class="separator"></div>
-      <h3>Developer @ MAKE NTNU</h3>
-      <h4>Java | Python | TypeScript | C#</h4>
+    <div class="contentWrapper">
+      <div class="left-side">
+        <h1 class="name">Hector Mendana Morales</h1>
+        <h3>Computer Science @ NTNU</h3>
+        <div class="separator"></div>
+        <h3>Developer @ MAKE NTNU</h3>
+        <h4>Java | Python | TypeScript | C#</h4>
+      </div>
+
+      <div class="right-side">
+        <h3 class="contact">hmmoralesdev@gmail.com</h3>
+        <a href="https://github.com/HectorMM5" class="GitHub">GitHub</a>
+      </div>
     </div>
 
-    <div class="right-side">
-      <h3 class="contact">hmmoralesdev@gmail.com</h3>
-      <a href="https://github.com/HectorMM5" class="GitHub">GitHub</a>
-    </div>
   </div>
 </template>
 
@@ -31,25 +35,28 @@ export default {
 .hero {
   position: relative;
   overflow: hidden;
+  background-color: var(--color-separator);
+  height: 95dvh;
+  width: 100%;
+  left: 0;
+}
+
+.contentWrapper {
   margin: auto;
   padding: 1rem;
   display: grid;
   grid-template-columns: 60% 40%;
-  background-color: var(--color-bg);
-  height: 85dvh;
-  width: 90dvw;
-  left: 0;
 }
 
 .left-side,
 .right-side {
-  margin: 12.5% 0 0 3%;
+  margin: 3% 0 0 3%;
   position: relative;
   z-index: 1;
 }
 
 @media (max-width: 800px) {
-  .hero {
+  .contentWrapper {
     grid-template-columns: 1fr;
   }
 
