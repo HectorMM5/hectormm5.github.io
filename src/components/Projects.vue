1ef2efa744fd3dc03f369aac7cf4b23532677839
Hector <hektor.mendana@gmail.com>
2026-03-10 14:41:11 +0100

    feat/added kitchen helper

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -8,7 +8,7 @@
         <h4 class="card-subtitle">{{ project.about }}</h4>
         <p class="card-techstack">{{ project.stack }}</p>
 
-          <ul class="card-bullets">
+        <ul class="card-bullets">
           <li
             class="card-bullet"
             v-for="(highlight, index) in project.highlights"
@@ -29,59 +29,71 @@ class Project {
   constructor(
     public name: string,
     public about: string,
-    public highlights: string[] = [], 
+    public highlights: string[] = [],
     public techStack: string[] = [],
   ) {}
 
   get stack(): string {
-    return this.techStack.join(" | ")
+    return this.techStack.join(' | ')
   }
 }
 
 const projects: Project[] = [
   new Project(
-    "Portfolio website",
+    'H.O.M.E System',
+    'Task-management app for small-medium size teams. Allows registration of tasks with multiple metrics; such as priority-level and workload rating. Tasks can be claimed by users or be automatically assigned.',
+    [
+      'Massively increased performance by implementation of in-memory H2-database',
+      'Designed and implemented external database',
+      'Participated in development of the task-allocation system',
+    ],
+    ['Java', 'MySQL', 'JavaFX', 'Maven'],
+  ),
+
+  new Project(
+    'BlueSky-clone',
+    'Social media app (work in progress), using the BlueSky API to get content. Web interface built with React. Automatic fetching of new posts white keeping local storage usage low.',
+    [
+      'Analyzed the API open-source code directly, due to lack of documentation',
+      'Utilizes both Python (backend API-interaction) and TypeScript (frontend), with communication using FastAPI (JSON syntax)',
+      'Learning project to familiarize myself with React, Docker and Docker Compose',
+    ],
+    ['React', 'Python', 'TypeScript', 'FastAPI', 'Docker', 'Docker Compose', 'HTML', 'CSS'],
+  ),
+
+  new Project(
+    'Board-game',
+    'Desktop board-game app with the classic games Snakes & Ladders and a simple Ludo game. Made with custom graphics, animations and dynamic board generation.',
+    [
+      'Abstract classes allow code reusability between games',
+      'Utilizes MVC structure and several design patterns, such as Observer, Factory, Singleton and Facade',
+      'Allows custom board import (JSON) and player profile statistics which are saved between sessions (CSV)',
+      'Built with extensive testing',
+    ],
+    ['Java', 'JavaFX', 'JUnit', 'JSON', 'CSV', 'Maven'],
+  ),
+
+  new Project(
+    'Kitchen helper',
+    'Cooking assistant app that allows registration of ingredients and recipes. Dynamically generates cooking suggestions based on the available items. Built with a CLI.',
+    [
+      'Focus on building with proper encapsulation, high cohesion and loose coupling',
+      'Handles different metrics correctly; such as litres and millilitres',
+      'User-friendly and interactive design',
+      'Built with high test coverage using JUnit, as well as withstanding thorough destructive testing',
+    ],
+    ['Java', 'JUnit', 'Maven'],
+  ),
+
+  new Project(
+    'Portfolio website',
     "The website you're in right now, aiming to present my work experience, education and projects. Built using Vue 3 and Vite, and hosted on GitHub Pages.",
     [
-      "Learning project to familiarize myself with Vue",
-      "TypeScript-enabled Vue components",
-      "Dynamic element generation allows easy addition of new education, project or work cards"
+      'Learning project to familiarize myself with Vue',
+      'TypeScript-enabled Vue components',
+      'Dynamic element generation allows easy addition of new education, project or work cards',
     ],
-    ["Vue", "Vite", "TypeScript", "HTML", "CSS"]
-  ),
-
-  new Project(
-    "H.O.M.E System", 
-    "Task-management app for small-medium size teams. Allows registration of tasks with multiple metrics; such as priority-level and workload rating. Tasks can be claimed by users or be automatically assigned.",
-    [
-      "Massively increased performance by implementation of in-memory H2-database",
-      "Designed and implemented external database",
-      "Participated in development of the task-allocation system"
-    ],
-    ["Java", "MySQL", "JavaFX", "Maven"]
-  ),
-
-  new Project(
-    "BlueSky-clone",
-    "Social media app (work in progress), using the BlueSky API to get content. Web interface built with React. Automatic fetching of new posts white keeping local storage usage low.",
-    [
-      "Analyzed the API open-source code directly, due to lack of documentation",
-      "Utilizes both Python (backend API-interaction) and TypeScript (frontend), with communication using FastAPI (JSON syntax)",
-      "Learning project to familiarize myself with React, Docker and Docker Compose",
-    ],
-    ["React", "Python", "TypeScript", "FastAPI", "Docker", "Docker Compose", "HTML", "CSS"]
-  ),
-
-  new Project(
-    "Board-game",
-    "Desktop board-game app with the classic games Snakes & Ladders and a simple Ludo game. Made with custom graphics, animations and dynamic board generation.",
-    [
-      "Abstract classes allow code reusability between games",
-      "Utilizes MVC structure and several design patterns, such as Observer, Factory, Singleton and Facade", 
-      "Allows custom board import (JSON) and player profile statistics which are saved between sessions (CSV)",
-      "Built with extensive testing",
-    ],
-    ["Java", "JavaFX", "JUnit", "JSON", "CSV", "Maven"]
+    ['Vue', 'Vite', 'TypeScript', 'HTML', 'CSS'],
   )
 ]
 
@@ -89,7 +101,7 @@ export default {
   name: 'Projects',
   data() {
     return {
-      projects: projects
+      projects: projects,
     }
   },
 }
